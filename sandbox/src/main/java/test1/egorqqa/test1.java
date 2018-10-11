955edaf2c0b344499f1242d523234b07e8389ee9
egorqqa <egor054@gmail.com>
2018-10-11 16:03:49 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/test1/egorqqa/test1.java
+++ sandbox/src/main/java/test1/egorqqa/test1.java
@@ -3,9 +3,24 @@ package test1.egorqqa;
 public class test1 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("world");
+      hello("user");
+      hello("Admin");
+double a = 4;
+double b = 6;
+      System.out.println((" +area(a, b)"));
+      double len = 5;
+      System.out.println("Площадь квадрата со стороной " + len +" = " + len*len);
+  }
+  public static void hello(String somebody) {
+
+
+      System.out.println("Hi2all!" + somebody);
   }
 
+  public static double area(double a, double b) {
+      return a*b;
+  }
 }
 
 
